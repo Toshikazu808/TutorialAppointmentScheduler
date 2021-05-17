2eb30f9a675a706551782fbdb030b10269a3f68a
Toshikazu808 <ryantkanno@gmail.com>
2021-05-17 14:25:25 -0700

    Bulk project upload. Includes models, views, controllers, viewModels, package js scripts, calendar js and css imports.

---
+++ AppointmentScheduling/Services/AppointmentService.cs
@@ -0,0 +1,58 @@
+﻿using AppointmentScheduling.Models;
+using AppointmentScheduling.Models.ViewModels;
+using AppointmentScheduling.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduling.Services
+{
+    public class AppointmentService : IAppointmentService
+    {
+        private readonly ApplicationDbContext _db;
+        public AppointmentService(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public List<DoctorVM> GetDoctorList()
+        {
+            /* 
+             * In our database we can see that our users tabel is called AspNetUsers.  We have to retrieve a list of users from this table and then join that to the AspNetRoles table using AspNetUserRoles as an intermediate table, and get only the users whos role is doctor.
+               How do we acces the AspNetUsers, AspNetRoles, and AspNetUserRoles tables?
+               Because they are included inside Identity, we can access them directly.
+               The name of the tables inside our db will be everything following AspNet 
+
+               Here we will be using query syntax.  This is more descriptive than method syntax when you are joining multiple tables.
+               
+             */
+
+            // This is known as projection, because we are not retrieving everything, but only certain columns.
+            var doctors = (from user in _db.Users
+                           join userRoles in _db.UserRoles on user.Id equals userRoles.UserId // Here we are joining tables using userRoles
+                           join roles in _db.Roles.Where(x => x.Name == Helper.Doctor) on userRoles.RoleId equals roles.Id
+                           select new DoctorVM
+                           {
+                               Id = user.Id,
+                               Name = user.Name
+                           }
+                           ).ToList();
+            return doctors;
+        }
+
+        public List<PatientVM> GetPatientList()
+        {
+            var patients = (from user in _db.Users
+                           join userRoles in _db.UserRoles on user.Id equals userRoles.UserId // Here we are joining tables using userRoles
+                           join roles in _db.Roles.Where(x => x.Name == Helper.Patient) on userRoles.RoleId equals roles.Id
+                           select new PatientVM
+                           {
+                               Id = user.Id,
+                               Name = user.Name
+                           }
+                           ).ToList();
+            return patients;
+        }
+    }
+}
---
+++ AppointmentScheduling/wwwroot/js/script-custom-calendar.js
@@ -0,0 +1,52 @@
+﻿// Documentation for FullCalendar can be found on fullcalendar.io
+
+$(document).ready(function () {
+    InitializeCalendar();
+});
+
+function InitializeCalendar() {
+    try {
+        var calendarEl = document.getElementById('calendar');
+        if (calendarEl != null) {
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                initialView: 'dayGridMonth',
+                headerToolbar: {
+                    left: 'prev,next,today',
+                    center: 'title',
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                },
+                selectable: true,
+                editable: false,
+                select: function (event) {
+                    onShowModal(event, null);
+                }
+            });
+            calendar.render();
+        }
+
+        //$('#calendar').fullCalendar({
+        //    timezone: false,
+        //    header: {
+        //        left: 'prev,next,today',
+        //        center: 'title',
+        //        right: 'month,agendaWeek,agendaDay'
+        //    },
+        //    selectable: true,
+        //    editable: false,
+        //    select: function (event) {
+        //        onShowModal(event, null);
+        //    }
+        //});
+    }
+    catch (e) {
+        alert(e);
+    }
+}
+
+function onShowModal(obj, isEventDetails) {
+    $("#appointmentInput").modal("show");
+}
+
+function onCloseModal() {
+    $("#appointmentInput").modal("hide");
+}
